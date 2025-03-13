62f2ab265a0df9b45c7c5e2de03f3b6c8cb870b2
pamiputra <fantarasa99@gmail.com>
2025-03-13 08:43:30 +0700

    update

--- main.go
+++ main.go
@@ -15,5 +15,11 @@ func main() {
 			"bio":  "Software Enginer",
 		})
 	})
+	router.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"name": "Pami Putra",
+			"bio":  "Software Enginer",
+		})
+	})
 	router.Run()
 }
