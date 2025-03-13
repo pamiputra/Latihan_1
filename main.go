f6b90fdf1fbf3cb759b5cd977c9df75744d46df0
pamiputra <fantarasa99@gmail.com>
2025-03-13 09:06:48 +0700

    update

--- main.go
+++ main.go
@@ -9,17 +9,21 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"name": "Pami Putra",
-			"bio":  "Software Enginer",
-		})
+	router.GET("/", rootHandler)
+	router.GET("/hello", helloHandler)
+	router.Run(":8080")
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Pami Putra",
+		"bio":  "Software Enginer",
 	})
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"name": "Pami Putra",
-			"bio":  "Software Enginer",
-		})
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"conten":    "Hello World",
+		"subtiitle": "Belajar Golang bareng pami putra",
 	})
-	router.Run()
 }
