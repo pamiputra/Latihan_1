87091df933d0f635ddfa6bbcde19503b4dbd6b64
pamiputra <fantarasa99@gmail.com>
2025-03-26 11:46:06 +0700

    error bnyak

--- main.go
+++ main.go
@@ -60,11 +60,16 @@ func postBooksHandler(c *gin.Context) {
 	err := c.ShouldBindJSON(&bookInput)
 	if err != nil {
 
+		errorMessages := []string{}
 		for _, e := range err.(validator.ValidationErrors) {
 			errorMessage := fmt.Sprintf("Error on field %s, condition: %s", e.Field(), e.ActualTag())
-			c.JSON(http.StatusBadRequest, errorMessage)
-			return
+			errorMessages = append(errorMessages, errorMessage)
+
 		}
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": errorMessages,
+		})
+		return
 
 	}
 
