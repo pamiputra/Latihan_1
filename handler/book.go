d6011ed8b8441fb37771a0e2895895ed604704d8
pamiputra <fantarasa99@gmail.com>
2025-03-26 14:32:52 +0700

    updatestruktur

---
+++ handler/book.go
@@ -0,0 +1,63 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"latihan-api/book"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+func RootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Pami Putra",
+		"bio":  "Software Enginer",
+	})
+}
+
+func HelloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"conten":    "Hello World",
+		"subtiitle": "Belajar Golang bareng pami putra",
+	})
+}
+
+func BooksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+
+	c.JSON(http.StatusOK, gin.H{"id": id, "title": title})
+}
+func QueryHandler(c *gin.Context) {
+	title := c.Query("title")
+	price := c.Query("price")
+
+	c.JSON(http.StatusOK, gin.H{"title": title, "price": price})
+}
+func PostBooksHandler(c *gin.Context) {
+	//title, price data yang akan diterima
+	var bookInput book.BookInput
+
+	err := c.ShouldBindJSON(&bookInput)
+	if err != nil {
+
+		errorMessages := []string{}
+		for _, e := range err.(validator.ValidationErrors) {
+			errorMessage := fmt.Sprintf("Error on field %s, condition: %s", e.Field(), e.ActualTag())
+			errorMessages = append(errorMessages, errorMessage)
+
+		}
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": errorMessages,
+		})
+		return
+
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"title": bookInput.Title,
+		"price": bookInput.Price,
+	})
+}
--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
+	"latihan-api/handler"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 )
 
 func main() {
@@ -14,69 +11,11 @@ func main() {
 
 	v1 := router.Group("/v1")
 
-	v1.GET("/", rootHandler)
-	v1.GET("/hello", helloHandler)
-	v1.GET("/books/:id/:title", booksHandler)
-	v1.GET("/query", queryHandler)
-	v1.POST("/books", postBooksHandler)
+	v1.GET("/", handler.RootHandler)
+	v1.GET("/hello", handler.HelloHandler)
+	v1.GET("/books/:id/:title", handler.BooksHandler)
+	v1.GET("/query", handler.QueryHandler)
+	v1.POST("/books", handler.PostBooksHandler)
 
-	router.Run()
-}
-
-func rootHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"name": "Pami Putra",
-		"bio":  "Software Enginer",
-	})
-}
-
-func helloHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"conten":    "Hello World",
-		"subtiitle": "Belajar Golang bareng pami putra",
-	})
-}
-
-func booksHandler(c *gin.Context) {
-	id := c.Param("id")
-	title := c.Param("title")
-
-	c.JSON(http.StatusOK, gin.H{"id": id, "title": title})
-}
-func queryHandler(c *gin.Context) {
-	title := c.Query("title")
-	price := c.Query("price")
-
-	c.JSON(http.StatusOK, gin.H{"title": title, "price": price})
-}
-
-type BookInput struct {
-	Title string      `json:"title" binding:"required"`
-	Price json.Number `json: "price" binding:"required, number"`
-}
-
-func postBooksHandler(c *gin.Context) {
-	//title, price data yang akan diterima
-	var bookInput BookInput
-
-	err := c.ShouldBindJSON(&bookInput)
-	if err != nil {
-
-		errorMessages := []string{}
-		for _, e := range err.(validator.ValidationErrors) {
-			errorMessage := fmt.Sprintf("Error on field %s, condition: %s", e.Field(), e.ActualTag())
-			errorMessages = append(errorMessages, errorMessage)
-
-		}
-		c.JSON(http.StatusBadRequest, gin.H{
-			"errors": errorMessages,
-		})
-		return
-
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"title": bookInput.Title,
-		"price": bookInput.Price,
-	})
+	router.Run(":8080")
 }
